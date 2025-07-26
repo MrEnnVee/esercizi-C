7e0bbeca7d017c4b8a771ccc414bc402adad8e30
MrEnnVee <nicola.vitale89@live.com>
2025-07-26 12:04:17 +0200

    Caricamento iniziale esercizi-C

---
+++ esercizio con if-else/eserzio if-else.c
@@ -0,0 +1,39 @@
+//esercizio per calcolo delle spese mensili basate su saldo iniziale, accrediti e debiti con un limite da imporre.
+
+#include <stdio.h>
+
+int main (void)
+
+{
+    printf ("%s", "Inserire saldo iniziale:   ");
+    int saldo; //saldo iniziale
+    scanf ("%d", &saldo);
+
+    printf ("%s", "Inserire addebiti:   ");
+    int spese; //spese varie
+    scanf ("%d", &spese);
+
+    printf ("%s", "Inserire accrediti:  ");
+    int accrediti; // stipendio in ingresso
+    scanf ("%d", &accrediti);
+
+    printf ("%s", "Inserire Limite:  ");
+    int limite; // limite massimo spesa mensile
+    scanf ("%d", &limite);
+
+    //calcolo
+    saldo = saldo + spese - accrediti;
+
+    //stampa limite e saldo
+    printf ("%s%d\n %s%d\n", "Limite: ", limite, "Saldo: ", saldo);
+
+    //avvisa se il saldo supera il limite
+    if (saldo > limite) {
+        printf ("%s", "Limite superato.");
+    } //fine if
+    else {
+        printf ("%s", "Limite non superato");
+    } //fine else
+
+
+}
---
+++ uso di printf-scanf-operazioni elementari/esercizio printf-scanf-operazioni elementari.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main ( void )
+
+{
+    int x, y; 
+    printf ( "Inserisci due numeri: ");
+    scanf ("%d%d", &x, &y );
+
+    printf ( "La somma è %d\n", x + y);
+    printf ( "La moltiplicazione è %d\n", x * y);
+    printf ( "La sottrazione è %d\n", x - y);
+    printf ( "La divisione è %d\n", x / y );
+    printf ( "Il resto è %d\n", x % y);
+
+}
